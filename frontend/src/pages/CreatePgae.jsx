5858d81e507a85aaebbafe56fdae411c561a76e8
TomG308 <tgutierrez308@gmail.com>
2024-09-08 14:47:33 -0600

    added to the create products and added zustand to manage state

--- frontend/src/pages/CreatePgae.jsx
+++ frontend/src/pages/CreatePgae.jsx
@@ -1,8 +1,55 @@
+import { Box, Container, useColorModeValue, VStack, Heading, Button, Input } from '@chakra-ui/react';
+import { useState } from 'react';
 import React from 'react'
 
 const CreatePgae = () => {
+
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: ""
+  });
+
+  const handleAddProduct = () => {
+    console.log(newProduct)
+  }
+
   return (
-    <div>CreatePgae</div>
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create new product
+        </Heading>
+
+        <Box w={"full"} bg={useColorModeValue("white", "gray.800")} p={6} rounded={"lg"} shadow={"md"}>
+          <VStack spacing={4}>
+            <Input 
+              type='text'
+              value={newProduct.name} 
+              placeholder='Product Name' 
+              name='name' 
+              onChange={(e) => setNewProduct({...newProduct, name: e.target.value})} 
+          />
+            <Input 
+              type='number'
+              value={newProduct.price} 
+              placeholder='Product Price' 
+              name='price' 
+              onChange={(e) => setNewProduct({...newProduct, price: e.target.value})} 
+            />
+            <Input 
+              value={newProduct.image} 
+              placeholder='image URL' 
+              name='image' 
+              onChange={(e) => setNewProduct({...newProduct, image: e.target.value})} 
+            />
+            <Button colorScheme='blue' onClick={handleAddProduct} w={'full'}>
+              Add Product
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
   )
 }
 
